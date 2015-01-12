174ec520930abfcb0189b8a9ef7a3aab3cf73d8e
WebCaos <lucianoubertini@gmail.com>
2015-01-12 11:55:54 +0100

    Edit Migrations

--- app/database/migrations/2014_11_21_155258_create_products_table.php
+++ app/database/migrations/2014_11_21_155258_create_products_table.php
@@ -17,13 +17,18 @@ class CreateProductsTable extends Migration {
             $table->string('Code')->primary();
             $table->string('BarCode');
             $table->string('Description');
+            $table->longtext('DescriptionHTML');
             $table->string('ProducerName');
             $table->string('SupplierProductCode');
+            $table->decimal('GrossPrice1', 15, 2);
+            $table->decimal('GrossPrice2', 15, 2);
+            $table->decimal('GrossPrice3', 15, 2);
             $table->decimal('GrossPrice4', 15, 2);
             $table->integer('AvailableQty');
             $table->decimal('Shipping', 15, 2);
             $table->string('NextAction');
             $table->string('AmazonStatus');
+            $table->string('eBayStatus');
             $table->timestamps();
         });
 	}
